5c1539753b9452f2534e7f99698df4c1b28f4304
nightfoxbmg <mishabalabanpw@gmail.com>
2023-07-24 17:20:56 +0300

    ignore added

---
+++ src/main/java/out/javabasic/lesson3/MainApp.java
@@ -0,0 +1,10 @@
+package out.javabasic.lesson3;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+}
